430b525b7f2d19fd059348a68485765e7be6674f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:32:47 -0300

    Created stacy/__log__.py at Timestamp: 2018-08-22 18:32:47

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 15:32:43.922 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  falatarzan = Texto(floresta, "Que fome !!!)
+                                             ^
+SyntaxError: EOL while scanning string literal
+'''},
