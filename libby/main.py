779a202a35bfedaecd9befc5804353ce9bd876fc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:06:02 -0300

    Saved libby/main.py at Timestamp: 2018-08-22 18:06:02

--- libby/main.py
+++ libby/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import INVENTARIO as inv 
 
 TARZAN = "https://banner2.kisspng.com/20180319/rpw/kisspng-toy-thumb-fictional-character-brown-hair-hand-tarzan-5ab053241310b6.3817319715215050600781.jpg"
-HOMEM ARANHA = "https://banner2.kisspng.com/20180619/fxt/kisspng-spider-man-television-fan-art-rendering-homem-aranha-5b289435b98b88.17202473152938603776.jpg"
+HOMEM_ARANHA = "https://banner2.kisspng.com/20180619/fxt/kisspng-spider-man-television-fan-art-rendering-homem-aranha-5b289435b98b88.17202473152938603776.jpg"
 PANTANO = "https://vignette.wikia.nocookie.net/monsterhunterespanol/images/2/2b/MHFU-Pantano.png/revision/latest?cb=20130503184351&path-prefix=es"
 CIDADE = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/CentroRJ.jpg/1200px-CentroRJ.jpg"
 
