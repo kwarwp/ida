0e6f2256b6c9535b91fb6c67ce8c81d84c06bf37
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:00:59 -0300

    Saved angie/main.py at Timestamp: 2018-08-22 19:00:58

--- angie/main.py
+++ angie/main.py
@@ -17,12 +17,12 @@ def criarcenas():
     floresta2.direita = cidade
     cidade.esquerda = floresta2 
     
-    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=200))
+    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=250))
     alice.entra(floresta)
     ealice = Texto(floresta, "Alice caminhava na floresta e estava perdida. Atormentada por seus pensamentos, imaginava todas coisas que poderiam estar acontecendo em sua casa.")
     alice.vai=ealice.vai
     
-    yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=60, top=150, width=50, height=100))
+    yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=60, top=150, width=50, height=70))
     yoda.entra(floresta2)
     eyoda = Texto(floresta2, "Então Alice, perdida na floresta, com seus pensamentos trevosos, encontra o Mestre Yoda.")
     yoda.vai=eyoda.vai
