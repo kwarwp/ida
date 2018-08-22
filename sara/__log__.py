8626f347825cc69a2739bb1967d2c2e169b10bb4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:28:39 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-22 18:28:39

--- sara/__log__.py
+++ sara/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: EOL while scanning string literal
                                                                                                                                                                                                      ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Aug 22 2018 15:28:39.147 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  ealice = Texto(deserto, " Em uma noite enluarada, Dona ALICE saiu para dar uma volta. Porém, durante o passeio, se perdeu no caminho e não enxergava nada a sua frente além de um pantamo assutador.
+                                                                                                                                                                                                      ^
+SyntaxError: EOL while scanning string literal
+'''},
