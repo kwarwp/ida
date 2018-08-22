685f9779a00f51e8764d8f543010f98c2b927127
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:50:51 -0300

    Created kathryn/main.py at Timestamp: 2018-08-22 17:50:51

---
+++ kathryn/main.py
@@ -0,0 +1,8 @@
+# ida.kathryn.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import INVENTARIO as inv 
+BARBIE  = "http://www.pngmart.com/files/4/Barbie-PNG-Image.png"
+TARZAN  = "https://banner2.kisspng.com/20180501/ite/kisspng-tarzan-jane-porter-film-etsy-drawing-jane-5ae85eb3d5d6f8.1259494515251780358759.jpg"
+HOMEM_ARANHA = "https://1.bp.blogspot.com/-fhZPiXXAr9c/V_-RoY-A8ZI/AAAAAAAAC04/09OJgAdU7QE3gUDGgVAsuqGU1q6JQI94ACLcB/s1600/spiderman-al-psicologo.jpg"
+
+
