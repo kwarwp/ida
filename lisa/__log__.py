18646e492f8384590b0bd1869449b94407c9c23d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:32:43 -0300

    Saved lisa/__log__.py at Timestamp: 2018-08-22 18:32:43

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -6,3 +6,10 @@
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 15:32:44.202 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 26
+  barbie =Elemento(img= BARBIE, tit="Barbie", style=dict(left=150, top=150, width=60, height=200)
+                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
