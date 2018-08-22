e25b75842ce576a28ac54aba337c30a3d49dade9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:51:10 -0300

    Created callie/main.py at Timestamp: 2018-08-22 17:51:10

---
+++ callie/main.py
@@ -0,0 +1,8 @@
+# ida.callie.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import INVENTARIO as inv
+BARBIE = "https://i.imgur.com/21y2YUF.png"
+TARZAN = "https://i.imgur.com/zgUuDIe.png"
+HOMEM_ARANHA = "https://i.imgur.com/H5gr3qm.png"
+FLORESTA = "https://i.imgur.com/ZjwoYBc.jpg"
+CIDADE = "https://i.imgur.com/RqRv4YB.jpg"
