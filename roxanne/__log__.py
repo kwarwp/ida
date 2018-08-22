84a1b7108d5b71058f7c03839e7c432434173345
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:42:45 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-22 19:42:45

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -557,3 +557,21 @@ UnboundLocalError: local variable 'tarzan' referenced before assignment
     tarzan.espera(1)
 UnboundLocalError: local variable 'tarzan' referenced before assignment
 '''},
+{'date': 'Wed Aug 22 2018 16:42:45.996 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    criarcenas()
+  module <module> line 44
+    tarzan.vai()
+UnboundLocalError: local variable 'tarzan' referenced before assignment
+'''},
