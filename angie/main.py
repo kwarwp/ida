dad2637f3198eced4d174f7026876e8126411ed5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:57:12 -0300

    Saved angie/main.py at Timestamp: 2018-08-22 18:57:12

--- angie/main.py
+++ angie/main.py
@@ -5,23 +5,28 @@ ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJK
 MESTRE_YODA = "https://vignette.wikia.nocookie.net/disney/images/9/95/Master_Yoda.png/revision/latest?cb=20161024220430&path-prefix=pt-br"
 CINDERELA = "https://4.bp.blogspot.com/-c_4RlOC4vRk/WD8fPzbqbcI/AAAAAAAACA0/1ian8CfRW9o-HTtSpVWOU2AG1TmmkUY3QCLcB/s1600/Cinderella-Transparent-PNG.png"
 FLORESTA = "https://vignette.wikia.nocookie.net/producaocultural/images/9/96/Floresta_Sombria.png/revision/latest?cb=20110605015107&path-prefix=pt-br" 
+FLORESTA2 = "https://vignette.wikia.nocookie.net/producaocultural/images/9/96/Floresta_Sombria.png/revision/latest?cb=20110605015107&path-prefix=pt-br" 
 CIDADE = "https://www.rawstory.com/wp-content/uploads/2014/10/New-York-Central-Park-autumn-Shutterstock-800x430.png"
 
 def criarcenas():
     floresta =Cena(img=FLORESTA)
+    floresta2 =Cena(img=FLORESTA2)
     cidade =Cena(img=CIDADE) 
-    floresta.direita = cidade
-    cidade.esquerda = floresta 
+    floresta.direita = floresta2
+    floresta2.esquerda = floresta 
+    floresta2.direita = cidade
+    cidade.esquerda = floresta2 
     
     alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=200))
     alice.entra(floresta)
     ealice = Texto(floresta, " Alice caminhava na floresta e estava perdida.")
+    alice.vai=ealice.vai
     ealice = Texto(floresta, " Atormentada por seus pensamentos, imaginava todas coisas que poderiam estar acontecendo em sua casa.")
     alice.vai=ealice.vai
     
     yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=150, top=150, width=50, height=150))
-    yoda.entra(floresta)
-    eyoda = Texto(floresta, "Então Alice, perdida na floresta, com seus pensamentos trevosos, encontra o Mestre Yoda.")
+    yoda.entra(floresta2)
+    eyoda = Texto(floresta2, "Então Alice, perdida na floresta, com seus pensamentos trevosos, encontra o Mestre Yoda.")
     yoda.vai=eyoda.vai
     #cinderela =Elemento(img= CINDERELA, tit="Cinderela", style=dict(left=150, top=150, width=60, height=200))
     
