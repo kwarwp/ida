e801f973c54a9c1272846608887f7da69496585b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:51:32 -0300

    Saved naomi/__log__.py at Timestamp: 2018-08-22 18:51:31

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -197,3 +197,21 @@ UnboundLocalError: local variable 'cidade' referenced before assignment
     Tarzan =Elemento(img= Tarzan, tit="Ex-marido da Jane", style=dict(left=150, top=150, width=60,height=200))
 UnboundLocalError: local variable 'Tarzan' referenced before assignment
 '''},
+{'date': 'Wed Aug 22 2018 15:51:30.834 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 35
+    criarcenas()
+  module <module> line 22
+    tarzan.vai=eTarzan.vai
+NameError: name 'eTarzan' is not defined
+'''},
