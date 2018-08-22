99da16ee6924dbb0edf0541ef5d947ea613790d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:50:40 -0300

    Created roxanne/main.py at Timestamp: 2018-08-22 17:50:40

---
+++ roxanne/main.py
@@ -0,0 +1,23 @@
+# ida.roxanne.main.py
+#Em uma bela tarde a Alice ficou perdida no pântano.
+#Quando foi encontrada pelo Tarzan
+#Os dois voltando ao castelo, encontraram o Homem-aranha.
+#Onde o Homem aranha estava passeando com a Barbie na cidade.
+#Quando o homem aranha contou para a Barbie que era filho do Yoda.
+#Eles viveram felizes para sempre
+
+
+from  _spy.vitolino.main import Cena, Elemento, Texto
+from _syp.vitolino.main import INVENTARIO as inv
+
+HOMEM ARANHA =  "https://vignette.wikia.nocookie.net/marvel/images/2/2e/Homem-aranha.png/revision/latest?cb=20141112195415&path-prefix=pt-br"
+
+TARZAN = 
+
+YODA = "https://banner2.kisspng.com/20180202/lzq/kisspng-yoda-darth-maul-star-wars-jedi-star-wars-transparent-background-5a7487326a2b16.7780970115175862264349.jpg"
+
+BARBIE = 
+
+ALICE = "https://icon2.kisspng.com/20180327/uue/kisspng-alice-liddell-alice-s-adventures-in-wonderland-the-alice-in-wonderland-5ab9ee8c10bb20.5426997015221346680685.jpg"
+
+
