2c03eee1b116b56f58e59303fafbca60f78e7d4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:30:22 -0300

    Saved samantha/main.py at Timestamp: 2018-08-22 19:30:21

--- samantha/main.py
+++ samantha/main.py
@@ -4,6 +4,7 @@ from _spy.vitollino.main import INVENTARIO as inv
 TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
 CINDERELA = "https://cdn.awsli.com.br/239/239279/produto/7143495/totens-displays-cinderela-64583d24.jpg"
 ALICE = "https://upload.wikimedia.org/wikipedia/pt/thumb/7/72/Alice_Disney.jpg/220px-Alice_Disney.jpg"
+ALICEE = "https://upload.wikimedia.org/wikipedia/pt/thumb/7/72/Alice_Disney.jpg/220px-Alice_Disney.jpg"
 HOMEM_ARANHA = "https://img.elo7.com.br/product/original/11FBB76/peso-de-porta-homem-aranha-spider-man-presentes.jpg"
 FLORESTA = "https://img.elo7.com.br/product/zoom/10C700C/painel-floresta-g-frete-gratis-painel-impresso.jpg"
 CASA = "http://1.bp.blogspot.com/-7CEY5DMNSDU/UnLHiLLQO7I/AAAAAAAAAUA/3xrMlvUTXzo/s1600/floresta-381288.jpg"
@@ -24,13 +25,13 @@ def criarcenas():
     cinderela =Elemento(img= CINDERELA, tit="Cinderela", style=dict(left=150, top=150, width=60,height=200)) 
     cinderela.entra(cha)
     
-    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60,height=200))
-    alice.entra(cha)
+    alicee =Elemento(img= ALICEE, tit="Alice", style=dict(left=200, top=150, width=60,height=200))
+    alicee.entra(cha)
     
-    tarzan =Elemento(img= TARZAN, tit="Tarzan", style=dict(left=150, top=150, width=60,height=200))
+    tarzan =Elemento(img= TARZAN, tit="Tarzan", style=dict(left=80, top=150, width=60,height=200))
     tarzan.entra(cha)
-    ealice = Texto(cha, ".")
-    alice.vai=ealice.vai
+    ealicee = Texto(cha, ".")
+    alicee.vai=ealicee.vai
     
     ecinderela = Texto(cha, " boa tarde!")
     cinderela.vai=ecinderela.vai
@@ -38,6 +39,7 @@ def criarcenas():
     etarzan = Texto(cha, " Ola!")
     tarzan.vai=etarzan.vai
     
+    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=200, top=150, width=60,height=200))
     alice.entra(floresta)
     ealice = Texto(floresta, " vamos para casa!")
     alice.vai=ealice.vai
