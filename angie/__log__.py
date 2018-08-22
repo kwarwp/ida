8bb7017957194cc9621320b4a07eb304fa849407
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:31:40 -0300

    Created angie/__log__.py at Timestamp: 2018-08-22 18:31:40

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 15:31:39.229 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  ealice = Texto(floresta, "Alice caminhava na floresta e estava perdida. Atormentada por seus pensamentos, imaginava todas coisas que poderiam estar acontecendo em sua casa."
+                                                                                                                                                                                     ^
+SyntaxError: invalid syntax
+'''},
