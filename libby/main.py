42b96bc941599485f9d025d561f64b06a1511562
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:53:19 -0300

    Saved libby/main.py at Timestamp: 2018-08-22 18:53:19

--- libby/main.py
+++ libby/main.py
@@ -6,7 +6,7 @@ TARZAN = "https://banner2.kisspng.com/20180319/rpw/kisspng-toy-thumb-fictional-c
 HOMEM_ARANHA = "https://static3.tcdn.com.br/img/img_prod/460977/pre_venda_boneco_homem_aranha_spider_man_homem_aranha_de_volta_ao_lar_spider_man_homecoming_movie_ma_21001_1_20170619150009.jpg"
 PANTANO = "https://vignette.wikia.nocookie.net/monsterhunterespanol/images/2/2b/MHFU-Pantano.png/revision/latest?cb=20130503184351&path-prefix=es"
 CIDADE = "http://www.revistaadventista.com.br/wp-content/uploads/2016/01/Campo-ou-cidade-1024x683.jpg"
-LATA_DE_LIXO = "http://clipart.coolclips.com/480/vectors/tf05169/CoolClips_vc011404.png"
+LATA_DE_LIXO = "http://clipart.coolclips.com/480/vectors/tf05052/CoolClips_envi0011.png"
 def criarcenas():
     cidade =Cena(img=CIDADE)
     pantano =Cena(img=PANTANO)
