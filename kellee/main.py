078ed298ed713a6c2cade995d3cd2a77825daa7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:59:49 -0300

    Saved kellee/main.py at Timestamp: 2018-08-22 17:59:49

--- kellee/main.py
+++ kellee/main.py
@@ -3,4 +3,8 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitolilino.main import INVENTARIO as inv
 TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
 HOMEM_ARANHA = "https://http2.mlstatic.com/adesivo-homem-aranha-recortado-cartela-1x1-D_NQ_NP_14475-MLB2822142047_062012-F.jpg"
-BARBIE = "
+BARBIE = "https://purepng.com/public/uploads/large/purepng.com-barbiebarbiefashion-dolltoybusinesswomanbild-lilli-1701528526670ltdca.png"
+FLORESTA = "https://img.elo7.com.br/product/original/12F2BDC/painel-de-festa-floresta-7-decoracao.jpg"
+CASTELO = "https://cdn.shopify.com/s/files/1/2097/9875/products/Square-Disney-Castle_2734d77e-37c4-4ba3-abac-b41afcd0b753_1024x1024.png?v=1513204613"
+PANTANO = "http://pluspng.com/img-png/swamp-png-hd-full-resolution-pluspng-com-png-swamp-1441.png"
+MESA = "https://media-cdn.tripadvisor.com/media/photo-s/05/db/4c/20/hotel-fazenda-lagoa-azul.jpg"
