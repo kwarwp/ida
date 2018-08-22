18ff18652f1a5d210cd5606fba8955ccd93e0ed6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:38:55 -0300

    Saved callie/main.py at Timestamp: 2018-08-22 18:38:55

--- callie/main.py
+++ callie/main.py
@@ -12,17 +12,17 @@ def criarcenas():
     cidade =Cena(img=CIDADE)
     floresta.direita = cidade
     
-    homem_aranha =Elemento(img=HOMEM_ARANHA, tit="homemAranha", style=dict(left=150, top=150, width=60, height=200)
+    homem_aranha =Elemento(img=HOMEM_ARANHA, tit="homemAranha", style=dict(left=150, top=150, width=60, height=200))
     homem_aranha.entra(floresta)
     falahomem_aranha = Texto(floresta, "Nossa! que isso? Estou numa floresta... como vim parar aqui?")
     homem_aranha.vai = falahomem_aranha.vai
     
-    tarzan =Elemento(img=TARZAN, tit="tarzan", style=dict(left=150, top=150, width=60, height=200)
+    tarzan =Elemento(img=TARZAN, tit="tarzan", style=dict(left=150, top=150, width=60, height=200))
     tarzan.entra(cidade)
     falatarzan = Texto(cidade, "Poxa o homem-aranha disse que iria logo ali rapinho e estou aqui trancado no apartamento dele mais de 5h!")
     tarzan.vai = falatarzan.vai
     
-    barbie = Elemento(img=BARBIE, tit="barbie", style=dict(left=150, top=150, width=60, height=200)
+    barbie = Elemento(img=BARBIE, tit="barbie", style=dict(left=150, top=150, width=60, height=200))
     barbie.entra(cidade)
     falabarbie = Texto(cidade, "Oi, homem-aranha!!! poderia ir numa festa comigo?")
     barbie.vai = falabarbie.vai
