9b4ad1d43ce3a1432799d6f0ceb7a1ee451c8aac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:33:35 -0300

    Saved kristen/__log__.py at Timestamp: 2018-08-22 18:33:34

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -6,3 +6,10 @@
                                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 15:33:34.652 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  tarzan = Elemento(img= TARZAN, tit="Tarzan", style=dict(left=150, top=150, width=60, height=200)
+                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
