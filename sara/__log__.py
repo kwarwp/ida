7710caf91e7971140a136abb1a43dee33b141f1a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:28:01 -0300

    Created sara/__log__.py at Timestamp: 2018-08-22 18:28:01

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 15:27:59.657 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  alice = Texto(deserto, " Em uma noite enluarada, Dona ALICE saiu para dar uma volta. Porém, durante o passeio, se perdeu no caminho e não enxergava nada a sua frente além de um pantamo assutador.
+                                                                                                                                                                                                     ^
+SyntaxError: EOL while scanning string literal
+'''},
