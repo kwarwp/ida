b983cb85104f8ff5fb4a9e165c3ec2ebdcdcbc4a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:13:56 -0300

    Created samantha/__log__.py at Timestamp: 2018-08-22 19:13:56

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 16:13:54.976 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60,height=200))
+  ^
+IndentationError: unexpected indent
+'''},
