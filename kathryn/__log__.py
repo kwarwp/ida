8954b712c004b9a1584d79599c47ab45ede72ad5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:14:05 -0300

    Created kathryn/__log__.py at Timestamp: 2018-08-22 18:14:04

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 15:14:04.881 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  barbie =Elemento(img= BARBIE, tit="Barbie", style=dict(left=150, top=150, width=60,height=200)
+                  ^
+SyntaxError: Unbalanced bracket (
+'''},
