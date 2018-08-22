2aaf0758b9dd960fc152e412fd96b5fc7cdcd3fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:51:04 -0300

    Created lisa/__log__.py at Timestamp: 2018-08-22 17:51:04

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 14:51:03.107 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  HOMEN ARANHA = "http://unuk.es/en/product/marvel-kids-sleeping-bag-by-selkbag/"
+         ^
+SyntaxError: invalid syntax
+'''},
