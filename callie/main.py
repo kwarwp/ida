c98349d5e2574d1d217f650b34e950a3a6a8bcaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:37:30 -0300

    Saved callie/main.py at Timestamp: 2018-08-22 18:37:29

--- callie/main.py
+++ callie/main.py
@@ -8,16 +8,16 @@ FLORESTA = "https://i.imgur.com/ZjwoYBc.jpg"
 CIDADE = "https://i.imgur.com/RqRv4YB.jpg"
 
 def criarcenas():
-    floresta = Cena(img=FLORESTA)
-    cidade = Cena(img=CIDADE)
+    floresta =Cena(img=FLORESTA)
+    cidade =Cena(img=CIDADE)
     floresta.direita = cidade
     
-    homem_aranha = Elemento(img=HOMEM_ARANHA, tit="homemAranha", style=dict(left=150, top=150, width=60, height=200)
+    homem_aranha =Elemento(img=HOMEM_ARANHA, tit="homemAranha", style=dict(left=150, top=150, width=60, height=200)
     homem_aranha.entra(floresta)
     falahomem_aranha = Texto(floresta, "Nossa! que isso? Estou numa floresta... como vim parar aqui?")
     homem_aranha.vai = falahomem_aranha.vai
     
-    tarzan = Elemento(img=TARZAN, tit="tarzan", style=dict(left=150, top=150, width=60, height=200)
+    tarzan =Elemento(img=TARZAN, tit="tarzan", style=dict(left=150, top=150, width=60, height=200)
     tarzan.entra(cidade)
     falatarzan = Texto(cidade, "Poxa o homem-aranha disse que iria logo ali rapinho e estou aqui trancado no apartamento dele mais de 5h!")
     tarzan.vai = falatarzan.vai
