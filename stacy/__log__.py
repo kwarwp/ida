ab1a03deba5eb6c33adb743393195fdf5854dbdc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:52:13 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-22 18:52:13

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -20,3 +20,21 @@ SyntaxError: EOL while scanning string literal
                                              ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Aug 22 2018 15:52:12.346 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    criarcenas()
+  module <module> line 30
+    alice.entra (mesa)
+NameError: name 'alice' is not defined
+'''},
