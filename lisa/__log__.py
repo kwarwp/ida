88a5ee6d5ba09cbe9baac65ccfd2e80d841fec0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:02:02 -0300

    Saved lisa/__log__.py at Timestamp: 2018-08-22 19:02:02

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -81,3 +81,21 @@ NameError: name 'apartamento' is not defined
     aranha.entra = earanha.vai
 UnboundLocalError: local variable 'earanha' referenced before assignment
 '''},
+{'date': 'Wed Aug 22 2018 16:02:03.78 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 54
+    criarcenas()
+  module <module> line 31
+    tarzan1 =Elemento(img= TARZAN, tit="Tarzan", style= dict(left=100, top=100, width=70, heigh=200))
+NameError: name 'TARZAN' is not defined
+'''},
