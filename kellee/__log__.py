c53387983dafdf8bc79ccd54a4d191680f184fa6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:50:53 -0300

    Created kellee/__log__.py at Timestamp: 2018-08-22 17:50:53

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 14:50:52.948 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  BARBIE = "
+            ^
+SyntaxError: EOL while scanning string literal
+'''},
