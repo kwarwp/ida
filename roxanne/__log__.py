c01e08e8a4aa9537e7b263be4d3905809b289480
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:51:17 -0300

    Created roxanne/__log__.py at Timestamp: 2018-08-22 17:51:17

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 14:51:16.151 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  HOMEM ARANHA =  "https://vignette.wikia.nocookie.net/marvel/images/2/2e/Homem-aranha.png/revision/latest?cb=20141112195415&path-prefix=pt-br"
+         ^
+SyntaxError: invalid syntax
+'''},
