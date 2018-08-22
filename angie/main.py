3d20f8fcd68d0e9cbb5840d906f3a512a0ea7a63
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:13:29 -0300

    Saved angie/main.py at Timestamp: 2018-08-22 19:13:28

--- angie/main.py
+++ angie/main.py
@@ -20,28 +20,22 @@ def criarcenas():
     alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=250))
     alice.entra(floresta)
     ealice = Texto(floresta, "Alice caminhava na floresta e estava perdida. Atormentada por seus pensamentos, imaginava todas coisas que poderiam estar acontecendo em sua casa.")
+    ealice = Texto(floresta2, "Então Alice, perdida na floresta, com seus pensamentos trevosos, encontra o Mestre Yoda.")
     alice.vai=ealice.vai
     
+    #alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=250))
+    #alice.entra(floresta2)
     
+    #alice.vai=ealice.vai
+    yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=60, top=150, width=50, height=70))
+    yoda.entra(floresta2)
+    eyoda = Texto(floresta2, "Vendo a aflição da menina, aconselha que ela tem que tirar férias na cidade")
+    yoda.vai=eyoda.vai
     #cinderela =Elemento(img= CINDERELA, tit="Cinderela", style=dict(left=150, top=150, width=60, height=200))
     
     
     
     floresta.vai()
-    
-    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=250))
-    alice.entra(floresta2)
-    ealice = Texto(floresta2, "Então Alice, perdida na floresta, com seus pensamentos trevosos, encontra o Mestre Yoda.")
-    alice.vai=ealice.vai
-    yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=60, top=150, width=50, height=70))
-    yoda.entra(floresta2)
-    eyoda = Texto(floresta2, "Vendo a aflição da menina, aconselha que ela tem que tirar férias na cidade")
-    yoda.vai=eyoda.vai
-    
-    
-    floresta2.vai()
-    
-    
 criarcenas()
     
  
