43a849be97b3d0da25fd596c54aede7473f7a20f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:02:15 -0300

    Saved sara/main.py at Timestamp: 2018-08-22 19:02:14

--- sara/main.py
+++ sara/main.py
@@ -12,11 +12,16 @@ def criarcenas():
     avenida =Cena(img=AVENIDA)
     pantano.direita = avenida
     
-    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60,height=10))
+    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60,height=200))
     alice.entra(pantano)
     falaalice = Texto(pantano, " Em uma noite enluarada, Dona ALICE saiu para dar uma volta. Porém, durante o passeio, se perdeu no caminho e não enxergava nada a sua frente além de um pantamo assutador.Pensando em como sair daquela enrrascada, a pequena Alice não teve outra opção do que rezar, quando de repente...")
     alice.vai=falaalice.vai
     pantano.vai()
+criarcenas()
+
+   pantano =Elemento(img= PANTANO, tit="pantano", style=dict(left=150, top=150, width=60,height=200))
+    pantano.entra(pantano)
+          pantano.vai()
 criarcenas()
     
     
