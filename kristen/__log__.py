35e533627116a6eb0f0a81068bf6e3b31991b344
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:33:04 -0300

    Created kristen/__log__.py at Timestamp: 2018-08-22 18:33:04

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 22 2018 15:33:02.717 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  tarzan =Elemento(img= TARZAN, tit="Tarzan", style=dict(left=150, top=150, width=60, height=200)
+                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
