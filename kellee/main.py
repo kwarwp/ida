6cfffb2c3141ffbbfa805ffeed9dbdf1606fbf60
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:50:37 -0300

    Created kellee/main.py at Timestamp: 2018-08-22 17:50:37

---
+++ kellee/main.py
@@ -0,0 +1,6 @@
+# ida.kellee.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitolilino.main import INVENTARIO as inv
+TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
+HOMEM_ARANHA = "https://http2.mlstatic.com/adesivo-homem-aranha-recortado-cartela-1x1-D_NQ_NP_14475-MLB2822142047_062012-F.jpg"
+BARBIE = "
