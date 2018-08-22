4c9a9ded41cdc3e66e4d942d35833bd8c9cc64eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:58:06 -0300

    Created naomi/main.py at Timestamp: 2018-08-22 17:58:06

---
+++ naomi/main.py
@@ -0,0 +1,10 @@
+# ida.naomi.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import INVENTARIO as inv
+Cidade = "https://thumbs.dreamstime.com/b/arquitectura-da-cidade-azul-34467258.jpg"
+Esgoto = "https://forbes.uol.com.br/wp-content/uploads/2018/04/esgoto-2.jpg"
+Apartamento = "http://s2.glbimg.com/feWuUX0z-7pLnz4wnUSaw_fpglA=/smart/e.glbimg.com/og/ed/f/original/2016/02/01/apartamento-samy-e-ricky-lapa360-01.jpg"
+Floresta = "https://ipc.digital/wp-content/uploads/2016/12/banho-de-floresta-640x360.jpg"
+Homem_Aranha = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
+Tarzan = "http://i82.photobucket.com/albums/j242/asia5/youngtarzan.png"
+Barbie = "https://i.pinimg.com/originals/e7/b9/75/e7b975861052f57c15095b4e85304adc.png"
