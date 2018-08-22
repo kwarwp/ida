c2183b6aab96a64763601a151ae656f090964f0a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:49:34 -0300

    Created courtney/main.py at Timestamp: 2018-08-22 17:49:33

---
+++ courtney/main.py
@@ -0,0 +1,5 @@
+# ida.courtney.main.py
+from _spy.vitolino.main import Cena, Elemento, Texto
+from _spy.vitolino.main import INVENTARIO as inv
+TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
+CINDERELA = "https://cdn.awsli.com.br/239/239279/produto/7143495/totens-displays-cinderela-64583d24.jpg"
