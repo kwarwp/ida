c67de6dee7688cb1b1c80377fd522ed9b4daad2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:46:32 -0300

    Saved grace/main.py at Timestamp: 2018-08-22 18:46:32

--- grace/main.py
+++ grace/main.py
@@ -8,7 +8,7 @@
 
 from  _spy.vitolino.main import Cena, Elemento, Texto
 from _syp.vitolino.main import INVENTARIO as inv
-HOMEM_ARANHA = "https://vignette.wikia.nocookie.net/marvel/images/2/2e/Homem-aranha.png/revision/latest?cb=20141112195415&path-prefix=pt-br"
+HOMEM_ARANHA = "https://img.elo7.com.br/product/original/11FBB76/peso-de-porta-homem-aranha-spider-man-presentes.jpg"
 TARZAN = "https://banner2.kisspng.com/20180501/ite/kisspng-tarzan-jane-porter-film-etsy-drawing-jane-5ae85eb3d5d6f8.1259494515251780358759.jpg"
 YODA = "https://banner2.kisspng.com/20180202/lzq/kisspng-yoda-darth-maul-star-wars-jedi-star-wars-transparent-background-5a7487326a2b16.7780970115175862264349.jpg"
 BARBIE = "https://icon2.kisspng.com/20180402/uaw/kisspng-barbie-a-fashion-fairytale-ballet-clip-art-barbie-5ac1e5f0320ce3.916858761522656752205.jpg"
