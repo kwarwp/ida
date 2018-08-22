8cf987f27ef5270f7ef8823ac43550ff058448d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:03:38 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-22 18:03:37

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 15:03:38.237 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  def porcena();
+               ^
+SyntaxError: invalid syntax
+'''},
