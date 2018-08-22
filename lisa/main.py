030284c991b781262244940ecd2fb9938a37e221
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:30:01 -0300

    Saved lisa/main.py at Timestamp: 2018-08-22 18:30:01

--- lisa/main.py
+++ lisa/main.py
@@ -1,23 +1,46 @@
 # ida.lisa.main.py
+# ida.kellee.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
-from _spy.vitollino.main import INVENTARIO as inv 
-HOMEM_ARANHA = "http://pngimg.com/uploads/spider_man/spider_man_PNG53.png"
-FLORESTA = "https://pre00.deviantart.net/e7ab/th/pre/f/2016/173/6/7/forest_clearing_background_png_stock_0075__2__copy_by_annamae22-da5zx2y.png"
-CIDADE = "https://s2.glbimg.com/1Wh9VrSZokI1CoRhHpdgeTapEnI=/e.glbimg.com/og/ed/f/original/2018/02/22/cidade.jpg"
-TARZAN = "https://therottingbat.files.wordpress.com/2014/01/tarzan_1999.jpg"
-BARBIE = "https://image.afcdn.com/story/20151130/-820959_w767h767c1cx510cy248.jpg"
-CANO = "https://thumbs.dreamstime.com/b/homem-no-t%C3%BAnel-com-oceano-sombrio-85554705.jpg"
-APARTAMENTO = "https://odis.homeaway.com/odis/listing/62148fe8-8822-45ed-830f-5f36b72cc271.c10.jpg"
+from _spy.vitollino.main import INVENTARIO as inv
+CHA = "https://vignette.wikia.nocookie.net/disneyprincesas/images/e/ef/Alice-in-wonderland-disneyscreencaps.com-5785.jpg/revision/latest?cb=20140822145715&path-prefix=pt-br"
+TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
+ARANHA = "https://lumiere-a.akamaihd.net/v1/images/usa_spider-man_chi_spider-man_r_d909a17b.png?region=0%2C0%2C600%2C600"
+FLORESTA = "https://img.elo7.com.br/product/zoom/12F2BAA/painel-de-festa-floresta-3-painel-em-tecido.jpg"
+BARBIE = "http://www.pngmart.com/files/4/Barbie-Transparent-Background.png"
+PANTANO = "https://gartic.com.br/imgs/mural/ca/catapimbador/pantano.png"
+CASTELO = "https://vignette.wikia.nocookie.net/barbie-movies/images/9/9f/Wpm56_Panel_28.jpg/revision/latest?cb=20140413120357"
 
 def criarcenas():
-    floresta = Cena(img=FLORESTA)
-    cidade = Cena(img=CIDADE)
+    cha =Cena(img=CHA)
+    floresta =Cena(FLORESTA)
+    pantano =Cena(PANTANO)    
+    castelo =Cena(CASTELO)
     
-    floresta.direita = cidade
+    cha.direita = floresta
+    floresta.esquerda = cha
+    floresta.direita = pantano
+    pantano.esquerda = floresta
+    pantano.direita = castelo
+    castelo.esquerda = pantano
     
-    homem_aranha = Elemento(img=HOMEM_ARANHA, tit="homem_aranha", style=dict(left=150, top=150, width=60, height=200)
-    homem_aranha.entra(floresta)
-    ehomem_aranha = Texto(floresta, "eu sabia que não devia ter bebido outra taça de vinho"
-    etarzan = Texto(apartamento, "minha primeira festa na cidade e eu acordo no apartamento de um desconhecido!"
-    #tarzan = Elemento(img=TARZAN, tit="tarzan", style=dict(left=150, top=150, width=60, height=200)
-    #barbie = Elemento(img=BARBIE, tit="barbie", style=dict(left=150, top=150, width=60, height=200)
+    barbie =Elemento(img= BARBIE, tit="Barbie", style=dict(left=150, top=150, width=60, height=200)
+    tarzan =Elemento(img= TARZAN, tit="Tarzan", style=dict(left=100, top=100, width=70, heigh=200)
+    aranha =Elemento(img= ARANHA, tit="Aranha", style=dict(left=170, top=100, width=60, height=180)
+    
+    tarzan.entra =(floresta)
+    aranha.entra =(cha)
+    aranha = Texto (cha, "eu mesmo!")
+    tarzan.entra =(cha)
+    tarzan = Texto (cha, "ouça esse som!")
+    barbie.entra =(pantano)
+    barbie = Texto (pantano, "SOCORRO, estou presa na areia movediça!")
+    tarzan.entra =(pantano)
+    tarzan = Texto (pantano, "eu te ajudo!")
+    barbie.entra =(castelo)
+    barbie = Texto (castelo, "venha para um almoço de agradecimento!)
+    
+    
+    
+    
+  
+
