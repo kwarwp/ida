8c0dcbfbb18c8ea7c0bb6668c39e97bc14fa7c22
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:24:04 -0300

    Saved libby/__log__.py at Timestamp: 2018-08-22 18:24:03

--- libby/__log__.py
+++ libby/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 15:24:03.212 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  homem_aranha =Elemento(img= HOMEM_ARANHA, tit="Homem_aranha", style=dict(left=150, top=150, width=60, height=200)
+                                                                                                                         ^
+SyntaxError: invalid syntax
+'''},
