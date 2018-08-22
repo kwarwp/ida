ba6b7f8c68b68d27454e6b53c243056e18774580
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:22:34 -0300

    Saved roxanne/main.py at Timestamp: 2018-08-22 19:22:34

--- roxanne/main.py
+++ roxanne/main.py
@@ -34,6 +34,7 @@ def criarcenas():
    
     pantano.vai()
     
+    pantano.direita = pantano
     
     alice = Elemento(img = ALICE, tit="Alice",style =dict(left=150 , top=150, width=60, height=200))
     alice.entra(pantano)
@@ -43,8 +44,9 @@ def criarcenas():
     tarzan = Elemento(img = TARZAN, tit="Tarzan", style =dict(left=50 , top=150, width=60, height=200))
     tarzan.entra(pantano)
     etarzan= Texto(pantano,"Não tema! Eu vim ajudar!")
+    tarzan.vai=etarzan.vai
     
-    pantano.direita = castelo
+    
 criarcenas()
     
     
