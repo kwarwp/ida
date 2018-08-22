37cc2103dd88bae8dec4c102fae6613723c36a9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:31:45 -0300

    Saved morgan/__log__.py at Timestamp: 2018-08-22 18:31:45

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -3,3 +3,7 @@
 'error': '''
 Exception: <SyntaxError: Unexpected token {>
 '''},
+{'date': 'Wed Aug 22 2018 15:31:45.375 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <SyntaxError: Unexpected token {>
+'''},
