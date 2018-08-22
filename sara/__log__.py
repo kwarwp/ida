49561135242b0b4eebc19a3d9244b1b75b033583
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:08:59 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-22 19:08:59

--- sara/__log__.py
+++ sara/__log__.py
@@ -187,3 +187,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Aug 22 2018 16:08:58.998 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  tarzan =Elemento(img= TARZAN, tit="Tarzan", style=dict(left=150, top=150, width=60,height=200))
+  ^
+IndentationError: unexpected indent
+'''},
