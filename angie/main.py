bcdf68b48eb178d7f33b0e721bf2ee95be64f98e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:31:35 -0300

    Saved angie/main.py at Timestamp: 2018-08-22 18:31:35

--- angie/main.py
+++ angie/main.py
@@ -13,6 +13,18 @@ def criarcenas():
     floresta.direita = praia 
     cidade.esquerda = floresta 
     
-    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=200)) 
-    yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=150, top=150, width=50, height=150))
-    cinderela =Elemento(img= CINDERELA, tit="Cinderela", style=dict(left=150, top=150, width=60, height=200)) 
+    alice =Elemento(img= ALICE, tit="Alice", style=dict(left=150, top=150, width=60, height=200))
+    alice.entra(floresta)
+    ealice = Texto(floresta, "Alice caminhava na floresta e estava perdida. Atormentada por seus pensamentos, imaginava todas coisas que poderiam estar acontecendo em sua casa."
+    alice.vai=ealice.vai
+    
+    
+    
+    
+    deserto.vai
+criarcenas()
+    
+   # yoda =Elemento(img= MESTRE_YODA, tit="Mestre Yoda", style=dict(left=150, top=150, width=50, height=150))
+   # yoda.entra(floresta)
+    
+   # cinderela =Elemento(img= CINDERELA, tit="Cinderela", style=dict(left=150, top=150, width=60, height=200)) 
