2ddb5e2b305fed5f6720e85ec20c73f1d6810548
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:50:37 -0300

    Created lisa/main.py at Timestamp: 2018-08-22 17:50:36

---
+++ lisa/main.py
@@ -0,0 +1,9 @@
+# ida.lisa.main.py
+# ida.kellee.main.py
+from spy.vitollino.main import Cena, Elemento, Texto
+from spy.vitollino.main import INVENTARIO as inv
+CHA "https://vignette.wikia.nocookie.net/disneyprincesas/images/e/ef/Alice-in-wonderland-disneyscreencaps.com-5785.jpg/revision/latest?cb=20140822145715&path-prefix=pt-br"
+TARZAN "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
+ARANHA "https://lumiere-a.akamaihd.net/v1/images/usa_spider-man_chi_spider-man_r_d909a17b.png?region=0%2C0%2C600%2C600"
+FLORESTA "https://img.elo7.com.br/product/zoom/12F2BAA/painel-de-festa-floresta-3-painel-em-tecido.jpg"
+
