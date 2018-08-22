f41d389ae49bbe89c1894bdc8795dd07d0829232
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:36:08 -0300

    Saved roxanne/main.py at Timestamp: 2018-08-22 19:36:08

--- roxanne/main.py
+++ roxanne/main.py
@@ -36,12 +36,12 @@ def criarcenas():
     
     alice = Elemento(img = ALICE, tit="Alice",style =dict(left=150 , top=150, width=60, height=200))
     alice.entra(pantano)
-    ealice = Texto(pantano,"help help")
+    ealice = Texto(pantano,"SOCORRRO")
     alice.vai=ealice.vai
     
     alice.espera(1000)
     
-    tarzan.mostrar()
+    tarzan.vai()
     
     tarzan = Elemento(img = TARZAN, tit="Tarzan", style =dict(left=50 , top=150, width=60, height=200))
     tarzan.entra(pantano)
