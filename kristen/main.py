c9dfb6ec509fb629b4c7b0bd67e6fa578ac9ad40
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:05:00 -0300

    Saved kristen/main.py at Timestamp: 2018-08-22 19:05:00

--- kristen/main.py
+++ kristen/main.py
@@ -1,13 +1,13 @@
 # ida.kristen.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import INVENTARIO as inv
-TARZAN = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwig1qWUm4HdAhWCjZAKHaNpA8QQjRx6BAgBEAU&url=http%3A%2F%2Fvsdebating.wikia.com%2Fwiki%2FTarzan_(Disney)&psig=AOvVaw2Yms2xSKqXh6cUQQ4ri9jR&ust=1535046275059726"
-BARBIE = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiz6pnHm4HdAhXKFpAKHWhOA3cQjRx6BAgBEAU&url=https%3A%2F%2Fbr.pinterest.com%2Fpin%2F530158187378766638%2F&psig=AOvVaw0GLqkSoSV7WlkSIAKEPhG0&ust=1535046303517432"
-ARANHA = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjerJDZm4HdAhVDj5AKHf_-ANQQjRx6BAgBEAU&url=http%3A%2F%2Finspiracaopop.com.br%2Fhomem-aranha%2F&psig=AOvVaw2PKmQftzZwouM3MzZgXhvD&ust=1535046418197694"
-GULOSEIMAS = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjeoKKPnIHdAhXKIZAKHZpWABQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.supimpagirl.com.br%2Fbeda-alice-disney-ilustras%2F&psig=AOvVaw1GemHq7yPWk2-r_pGdmvxW&ust=1535046534375529"
-CASTELO = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjIzaaqnIHdAhVJhpAKHbJsA90QjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.drama-total-fanon.wikia.com%2Fwiki%2FArquivo%3ACastelo.png&psig=AOvVaw0z7qQkd66UNZF8FLTJOsIw&ust=1535046582226979"
-PANTANO = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiy4969nIHdAhWLlJAKHc8tC2sQjRx6BAgBEAU&url=http%3A%2F%2Fpt.runescape.wikia.com%2Fwiki%2FFicheiro%3AP%25C3%25A2ntano_de_Mort_Myre.png&psig=AOvVaw0-DNFldM9eBA_Y_X14cyqs&ust=1535046628107721"
-FLORESTA = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiwysCOo4HdAhVDgJAKHTNDAoQQjRx6BAgBEAU&url=http%3A%2F%2Fpt-br.grandchase.wikia.com%2Fwiki%2FArquivo%3AFloresta_do_desafio_0.png&psig=AOvVaw1OJSdMoJWb5QyCj2F4uzFU&ust=1535048414320630"
+TARZAN = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXXq2vvGGCZRQ7F_8MeygorCuf-CtZF9CXfoUegrRcNnoqK_K8"
+BARBIE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUqpvOfTN1A1DupHXmSJa350IxMHIUe5ooBryqWdNPCdaH3FqrSVlJB5s"
+ARANHA = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY8V7Bbk-uUkzfdCFuAUb6rl66GHu7n1347Oh980sQXw27-UtB"
+GULOSEIMAS = "http://www.supimpagirl.com.br/wp-content/uploads/2017/08/alice-no-pa%C3%ADs-das-maravilhas-ilustras-disney5.png"
+CASTELO = "https://vignette.wikia.nocookie.net/drama-total-fanon/images/a/af/Castelo.png/revision/latest/scale-to-width-down/640?cb=20160117202039&path-prefix=pt-br"
+PANTANO = "https://vignette.wikia.nocookie.net/runescape2/images/3/3f/P%C3%A2ntano_de_Mort_Myre.png/revision/latest?cb=20120331195051&path-prefix=pt"
+FLORESTA = "https://vignette.wikia.nocookie.net/grandchase/images/8/8d/Floresta_do_desafio_0.png/revision/latest?cb=20120218225645&path-prefix=pt-br"
 
 def criarcenas():
     guloseimas =Cena(img=GULOSEIMAS)
@@ -35,5 +35,4 @@ def criarcenas():
     
     
     floresta.vai()
-    
 criarcenas()
