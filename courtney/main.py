be98ac67257d08883374854e3846199d09a47485
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:59:59 -0300

    Saved courtney/main.py at Timestamp: 2018-08-22 17:59:59

--- courtney/main.py
+++ courtney/main.py
@@ -4,4 +4,7 @@ from _spy.vitolino.main import INVENTARIO as inv
 TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
 CINDERELA = "https://cdn.awsli.com.br/239/239279/produto/7143495/totens-displays-cinderela-64583d24.jpg"
 ALICE = "https://upload.wikimedia.org/wikipedia/pt/thumb/7/72/Alice_Disney.jpg/220px-Alice_Disney.jpg"
-HOMEM ARANHA = "https://img.elo7.com.br/product/original/11FBB76/peso-de-porta-homem-aranha-spider-man-presentes.jpg"
+HOMEM_ARANHA = "https://img.elo7.com.br/product/original/11FBB76/peso-de-porta-homem-aranha-spider-man-presentes.jpg"
+FLORESTA = "https://img.elo7.com.br/product/zoom/10C700C/painel-floresta-g-frete-gratis-painel-impresso.jpg"
+CASA = "http://1.bp.blogspot.com/-7CEY5DMNSDU/UnLHiLLQO7I/AAAAAAAAAUA/3xrMlvUTXzo/s1600/floresta-381288.jpg"
+CASTELO = "https://png.pngtree.com/element_origin_min_pic/21/03/20/1656efb0f4aa630.jpg"
