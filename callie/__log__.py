317ffb69b2ed1b7a87e4a90b863ceb52f91cadb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:35:08 -0300

    Saved callie/__log__.py at Timestamp: 2018-08-22 18:35:08

--- callie/__log__.py
+++ callie/__log__.py
@@ -6,3 +6,10 @@
                                                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 15:35:08.349 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  homem_aranha = Elemento(img=HOMEM_ARANHA, tit="homemAranha", style=dict(left=150, top=150, width=60, height=200)
+                                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
