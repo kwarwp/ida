ea302b0d74aa3f17f996226d5be96e1d7f84c08a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:51:23 -0300

    Created angie/main.py at Timestamp: 2018-08-22 17:51:23

---
+++ angie/main.py
@@ -0,0 +1,5 @@
+# ida.angie.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import INVENTARIO as inv 
+ALICE = "https://icon2.kisspng.com/20180327/uue/kisspng-alice-liddell-alice-s-adventures-in-wonderland-the-alice-in-wonderland-5ab9ee8c10bb20.5426997015221346680685.jpg"
+MESTRE_YODA = "https://vignette.wikia.nocookie.net/disney/images/9/95/Master_Yoda.png/revision/latest?cb=20161024220430&path-prefix=pt-br"
