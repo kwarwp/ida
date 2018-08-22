a03a017cfccb200b3902282c335c2e35ae12519d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:36:30 -0300

    Saved kellee/main.py at Timestamp: 2018-08-22 18:36:30

--- kellee/main.py
+++ kellee/main.py
@@ -20,8 +20,8 @@ def criarcenas ():
     
     tarzan =Elemento(img= TARZAN, tit="Tarzan", style=dict(left=150, top=150, width=60,height=200))
     tarzan.entra(floresta)
-    homem aranha =Elemento(img= HOMEM_ARANHA, tit="Homem_Aranha", style=dict(left=150, top=150, width=60,heigth=200))
-    homem aranha.entra(mesa)
+    homem_aranha =Elemento(img= HOMEM_ARANHA, tit="Homem_Aranha", style=dict(left=150, top=150, width=60,heigth=200))
+    homem_aranha.entra(mesa)
     barbie =Elemento(img= BARBIE, tit="Barbie", style=dict(left=150, top=150
     barbie.entra(pantano)
     falabarbie = Texto(deserto, "Socorro! Socorro! Estou na areia movediça!!")
