60beaa3c0cc7ba76ef50c0ad31458c6958a03328
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:51:44 -0300

    Created stacy/main.py at Timestamp: 2018-08-22 17:51:44

---
+++ stacy/main.py
@@ -0,0 +1,6 @@
+# ida.stacy.main.py
+from _spy.vitollino.main import Cena, Elemento,Texto
+from _spy.vitollino.main import INVENTARIO as inv
+TARZAN = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj_4-XVm4HdAhXKGJAKHSPTAVEQjRx6BAgBEAU&url=http%3A%2F%2Fpt.disneyheroes.wikia.com%2Fwiki%2FTarzan_(personagem)&psig=AOvVaw0JpdfYW9IZHwGTpKg4Tblt&ust=1535046398062239"
+ALICE = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9ivuDnIHdAhVEI5AKHQRICyAQjRx6BAgBEAU&url=http%3A%2F%2Fdocecantinhodare.blogspot.com%2F2015%2F11%2Falice-no-pais-das-maravilhas-png.html&psig=AOvVaw3l-7liDDOM4pvAJKdqluh6&ust=1535046507401857"
+CHAPELEIRO = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiahK3RnIHdAhXFQpAKHRC-BWkQjRx6BAgBEAU&url=http%3A%2F%2Fmariafresa.net%2Fsingle%2F2265526.html&psig=AOvVaw2S7fwYVYWrVgnt-gYICKd1&ust=1535046650677203"
