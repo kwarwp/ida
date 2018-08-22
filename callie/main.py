af7f162bbde99c72126ab3326f5c691b5a1c95eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:33:31 -0300

    Saved callie/main.py at Timestamp: 2018-08-22 18:33:31

--- callie/main.py
+++ callie/main.py
@@ -20,10 +20,13 @@ def criarcenas():
     
     tarzan = Elemento(img=TARZAN, tit="tarzan", style=dict(left=150, top=150, width=60, height=200)
     tarzan.entra(cidade)
-    falatarzan = Texto(cidade, "Poxa o homem-aranha me deixou aqui no apartamento dele ")
+    falatarzan = Texto(cidade, "Poxa o homem-aranha disse que iria logo ali rapinho e estou aqui trancado no apartamento dele mais de 5h!")
     tarzan.vai = falatarzan.vai
     
-   #barbie = Elemento(img=BARBIE, tit="barbie", style=dict(left=150, top=150, width=60, height=200)
+    barbie = Elemento(img=BARBIE, tit="barbie", style=dict(left=150, top=150, width=60, height=200)
+    barbie.entra(cidade)
+    falabarbie = Texto(cidade, "Oi, homem-aranha!!! poderia ir numa festa comigo?")
+    barbie.vai = falabarbie.vai
    
    floresta.vai()
 criarcenas()
