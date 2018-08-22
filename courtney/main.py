0883e49f897ab9f473c45724a5972b6b41a0bfc2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:51:59 -0300

    Saved courtney/main.py at Timestamp: 2018-08-22 17:51:58

--- courtney/main.py
+++ courtney/main.py
@@ -3,3 +3,4 @@ from _spy.vitolino.main import Cena, Elemento, Texto
 from _spy.vitolino.main import INVENTARIO as inv
 TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest?cb=20170117061234"
 CINDERELA = "https://cdn.awsli.com.br/239/239279/produto/7143495/totens-displays-cinderela-64583d24.jpg"
+ALICE = "https://upload.wikimedia.org/wikipedia/pt/thumb/7/72/Alice_Disney.jpg/220px-Alice_Disney.jpg"
