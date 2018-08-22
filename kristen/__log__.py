d0654b2431dc359e521e8711dfab57ffd1bd3353
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:50:03 -0300

    Saved kristen/__log__.py at Timestamp: 2018-08-22 18:50:03

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -171,3 +171,21 @@ NameError: name 'texto' is not defined
     etarzan = texto (guloseimas, "Olha, o Homem Aranha")
 NameError: name 'texto' is not defined
 '''},
+{'date': 'Wed Aug 22 2018 15:50:03.10 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 36
+    criarcenas()
+  module <module> line 29
+    etarzan = texto (guloseimas, "Olha, o Homem Aranha")
+NameError: name 'texto' is not defined
+'''},
