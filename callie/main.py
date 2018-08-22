7992d5cbd4eaf8b047ca0cacb05ff33913cc89ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:29:30 -0300

    Saved callie/main.py at Timestamp: 2018-08-22 19:29:29

--- callie/main.py
+++ callie/main.py
@@ -1,6 +1,6 @@
 # ida.callie.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
-from _spy.vitollino.main import INVENTARIO as inv
+#from _spy.vitollino.main import INVENTARIO as inv
 BARBIE = "https://i.imgur.com/21y2YUF.png"
 TARZAN = "https://i.imgur.com/zgUuDIe.png"
 HOMEM_ARANHA = "https://i.imgur.com/H5gr3qm.png"
@@ -8,6 +8,7 @@ FLORESTA = "https://i.imgur.com/ZjwoYBc.jpg"
 CIDADE = "https://i.imgur.com/RqRv4YB.jpg"
 STYLE["width"] = 700
 STYLE["height"] = "650px"
+
 def criarcenas():
     floresta = Cena(img=FLORESTA)
     cidade = Cena(img=CIDADE)
