0a09ce92a5881ae3ad74b07270eb47d3edbc0738
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 15:04:55 -0300

    Saved libby/__log__.py at Timestamp: 2018-08-22 18:04:54

--- libby/__log__.py
+++ libby/__log__.py
@@ -6,3 +6,10 @@
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 15:04:54.354 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  HOMEM ARANHA = "https://banner2.kisspng.com/20180619/fxt/kisspng-spider-man-television-fan-art-rendering-homem-aranha-5b289435b98b88.17202473152938603776.jpg"
+         ^
+SyntaxError: invalid syntax
+'''},
