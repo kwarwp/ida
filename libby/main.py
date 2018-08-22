2b41163f3e3f47d3d1c6757e4da5fb6ecc56f46b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 14:55:01 -0300

    Created libby/main.py at Timestamp: 2018-08-22 17:55:01

---
+++ libby/main.py
@@ -0,0 +1,8 @@
+# ida.libby.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import INVENTARIO as inv 
+
+TARZAN = "https://banner2.kisspng.com/20180319/rpw/kisspng-toy-thumb-fictional-character-brown-hair-hand-tarzan-5ab053241310b6.3817319715215050600781.jpg"
+HOMEM ARANHA = "https://banner2.kisspng.com/20180619/fxt/kisspng-spider-man-television-fan-art-rendering-homem-aranha-5b289435b98b88.17202473152938603776.jpg"
+PANTANO = "https://vignette.wikia.nocookie.net/monsterhunterespanol/images/2/2b/MHFU-Pantano.png/revision/latest?cb=20130503184351&path-prefix=es"
+CIDADE = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/CentroRJ.jpg/1200px-CentroRJ.jpg"
