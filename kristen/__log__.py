3db50a1956022a22e4393b14147e29fff04af996
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 16:22:29 -0300

    Saved kristen/__log__.py at Timestamp: 2018-08-22 19:22:29

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -358,3 +358,21 @@ SyntaxError: invalid syntax
     barbie1.vai-ebarbie1.vai
 TypeError: unsupported operand types for -: 'function' and 'method'
 '''},
+{'date': 'Wed Aug 22 2018 16:22:29.672 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    criarcenas()
+  module <module> line 50
+    barbie2.entra (castelo)
+NameError: name 'barbie2' is not defined
+'''},
