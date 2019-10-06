3bec4fe18713ad6b985354485d5d968863506930
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 14:36:24 -0300

    Saved anastasia/main.py @2019-10-06 17:36

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,10 +1,1 @@
 # ida.anastasia.main.py
-from _spy.vitollino.main import Cena, Elemento, Texto
-from _spy.vitollino.main import INVENTARIO as inv
-cinderela = "https://4.bp.blogspot.com/-c_4RlOC4vRk/WD8fPzbqbcI/AAAAAAAACA0/1ian8CfRW9o-HTtSpVWOU2AG1TmmkUY3QCLcB/s1600/Cinderella-Transparent-PNG.png"
-tarzan = "https://banner2.kisspng.com/20180501/ite/kisspng-tarzan-jane-porter-film-etsy-drawing-jane-5ae85eb3d5d6f8.1259494515251780358759.jpg"
-homem aranha = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
-floresta = "https://vignette.wikia.nocookie.net/grandchase/images/8/8d/Floresta_do_desafio_0.png/revision/latest?cb=20120218225645&path-prefix=pt-br"
-casa = "https://images.emojiterra.com/google/android-oreo/512px/1f3e0.png"
-mesa de chá = "https://vignette.wikia.nocookie.net/disney/images/d/dc/Alice_Through_The_Looking_Glass%21_196.png/revision/latest?cb=20160511184926"
-
