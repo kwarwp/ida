b8ce98c23d89d2dd1193bc9cf3904c6d8752be31
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-06 09:35:56 -0300

    Saved morgan/main.py @2022-09-06 12:35

--- morgan/main.py
+++ morgan/main.py
@@ -1,3 +1,12 @@
 # ida.morgan.main.py
+from _spy.vitollino.main import Cena
+
+IMAGEM = "https://i.imgur.com/PvPDt89.jpg"
+cenaImagem = Cena(IMAGEM)
+cenaImagem.vai()
+
+mensagem = Texto(cenaImagem, txt = 'Cidade Maravilhosa')
+mensagem.vai()
+
 
     
