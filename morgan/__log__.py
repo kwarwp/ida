8aef5c465093d6c9816770b672ede33a3ce241a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-06 09:36:18 -0300

    Saved morgan/__log__.py @2022-09-06 12:36

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -113,3 +113,19 @@ NameError: name 'Texto' is not defined
     mensagem = Texto(cenaImagem, txt = 'Cidade Maravilhosa')
 NameError: name 'Texto' is not defined
 '''},
+{'date': 'Tue Sep 06 2022 09:36:17.719 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    mensagem = Texto(cenaImagem, txt = 'Cidade Maravilhosa')
+NameError: name 'Texto' is not defined
+'''},
